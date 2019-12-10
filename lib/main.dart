9616ad43a552a1eb0262875c59144ba73fc818d4
Roberto Borges <pedro.borges@souunit.com.br>
2019-12-10 09:37:34 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import "package:flutter/material.dart";
+
+
+void main() {
+  runApp(MaterialApp(
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+
+}
+
+TextEditingController weightController = TextEditingController();
+TextEditingController heightController = TextEditingController();
+
+String _infoText = "Informe os Dados";
+
+
+
+
+
+class _HomeState extends State<Home> {
+
+  void resetFields(){
+
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe os Dados";
+    });
+
+  }
+
+  void _calculate(){
+    double weight = double.parse(weightController.text);
+    double height = double.parse(heightController.text);
+    double imc = (weight / ((height/100) * 2));
+    setState(() {
+      if(imc < 18.6){
+        _infoText = "Abaixo do peso $imc";
+      }
+    });
+
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: resetFields)
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.perm_identity,
+              size: 125.0,
+              color: Colors.deepPurpleAccent,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: ("Peso (Kg)"),
+                labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontSize: 25,
+              ),
+              controller: weightController,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: ("Altura (Cm)"),
+                labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontSize: 25,
+              ),
+              controller: heightController,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: _calculate,
+                  child: Text("Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25)),
+                  textColor: Colors.white,
+                  color: Colors.deepPurpleAccent,
+                ),
+              ),
+            ),
+            Text(
+              _infoText,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
