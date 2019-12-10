e42e87396043d1e8094b5bdd3ae8bb945ebfdd3e
Roberto Borges <pedro.borges@souunit.com.br>
2019-12-10 10:05:56 -0300

    Calculo correto imc

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,25 @@ class _HomeState extends State<Home> {
   }
 
   void _calculate(){
+
     double weight = double.parse(weightController.text);
     double height = double.parse(heightController.text);
     double imc = (weight / ((height/100) * 2));
+
     setState(() {
-      if(imc < 18.6){
-        _infoText = "Abaixo do peso $imc";
+
+      if(imc < 18.5){
+        _infoText = "Abaixo do peso ${imc.toStringAsPrecision(4)}";
+      }else if(imc >= 18.5 && imc < 24.9){
+        _infoText = "Peso Ideal ${imc.toStringAsPrecision(4)}";
+      }else if(imc >= 24.9 && imc < 29.9){
+        _infoText = "Levemente Acima do Peso ${imc.toStringAsPrecision(4)}";
+      }else if(imc >= 29.9 && imc < 34.9){
+        _infoText = "Obesidade Grau I ${imc.toStringAsPrecision(4)}";
+      }else if(imc >= 34.9 && imc < 39.9){
+        _infoText = "Obesidade Grau II ${imc.toStringAsPrecision(4)}";
+      }else if(imc >= 40){
+        _infoText = "Obesidade Grau III ${imc.toStringAsPrecision(4)}";
       }
     });
 
