b544382a9c82c8a0dec8928f41bb6ac21f273a47
Roberto Borges <pedro.borges@souunit.com.br>
2019-12-10 10:44:16 -0300

    Calculo correto imc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 
-
 void main() {
   runApp(MaterialApp(
     home: new Home(),
@@ -10,7 +9,6 @@ void main() {
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
-
 }
 
 TextEditingController weightController = TextEditingController();
@@ -18,48 +16,38 @@ TextEditingController heightController = TextEditingController();
 
 String _infoText = "Informe os Dados";
 
-
-
-
-
 class _HomeState extends State<Home> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  void resetFields(){
-
+  void resetFields() {
     weightController.text = "";
     heightController.text = "";
     setState(() {
       _infoText = "Informe os Dados";
     });
-
   }
 
-  void _calculate(){
-
+  void _calculate() {
     double weight = double.parse(weightController.text);
     double height = double.parse(heightController.text);
-    double imc = (weight / ((height/100) * 2));
+    double imc = (weight / ((height / 100) * 2));
 
     setState(() {
-
-      if(imc < 18.5){
+      if (imc < 18.5) {
         _infoText = "Abaixo do peso ${imc.toStringAsPrecision(4)}";
-      }else if(imc >= 18.5 && imc < 24.9){
+      } else if (imc >= 18.5 && imc < 24.9) {
         _infoText = "Peso Ideal ${imc.toStringAsPrecision(4)}";
-      }else if(imc >= 24.9 && imc < 29.9){
+      } else if (imc >= 24.9 && imc < 29.9) {
         _infoText = "Levemente Acima do Peso ${imc.toStringAsPrecision(4)}";
-      }else if(imc >= 29.9 && imc < 34.9){
+      } else if (imc >= 29.9 && imc < 34.9) {
         _infoText = "Obesidade Grau I ${imc.toStringAsPrecision(4)}";
-      }else if(imc >= 34.9 && imc < 39.9){
+      } else if (imc >= 34.9 && imc < 39.9) {
         _infoText = "Obesidade Grau II ${imc.toStringAsPrecision(4)}";
-      }else if(imc >= 40){
+      } else if (imc >= 40) {
         _infoText = "Obesidade Grau III ${imc.toStringAsPrecision(4)}";
       }
     });
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -75,59 +63,63 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(
-              Icons.perm_identity,
-              size: 125.0,
-              color: Colors.deepPurpleAccent,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: ("Peso (Kg)"),
-                labelStyle: TextStyle(color: Colors.deepPurpleAccent),
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.perm_identity,
+                size: 125.0,
                 color: Colors.deepPurpleAccent,
-                fontSize: 25,
               ),
-              controller: weightController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: ("Altura (Cm)"),
-                labelStyle: TextStyle(color: Colors.deepPurpleAccent),
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.deepPurpleAccent,
-                fontSize: 25,
-              ),
-              controller: heightController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              child: Container(
-                height: 50.0,
-                child: RaisedButton(
-                  onPressed: _calculate,
-                  child: Text("Calcular",
-                      style: TextStyle(color: Colors.white, fontSize: 25)),
-                  textColor: Colors.white,
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: ("Peso (Kg)"),
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   color: Colors.deepPurpleAccent,
+                  fontSize: 25,
+                ),
+                controller: weightController,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: ("Altura (Cm)"),
+                  labelStyle: TextStyle(color: Colors.deepPurpleAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.deepPurpleAccent,
+                  fontSize: 25,
+                ),
+                controller: heightController,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: _calculate,
+                    child: Text("Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25)),
+                    textColor: Colors.white,
+                    color: Colors.deepPurpleAccent,
+                  ),
                 ),
               ),
-            ),
-            Text(
-              _infoText,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 25.0),
-            ),
-          ],
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(color: Colors.deepPurpleAccent, fontSize: 25.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
